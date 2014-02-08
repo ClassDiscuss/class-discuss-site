f79b45e6aedca2f6323623f506f6fe6175989313
Prateek Srivastava <f2prateek@gmail.com>
2014-02-08 16:40:42 -0700

    Add a views template

--- class_discuss_site/views.py
+++ class_discuss_site/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from models import Group
+
 # Create your views here.
 def groups(request):
-    return HttpResponse("Hello, world. You're at the groups index.")
+    groups = Group.objects.all()
+    context = {'groups': groups}
+    return render(request, 'class_discuss_site/groups.html', context)
 
 def group_detail(request, group_id):
     return HttpResponse("You're looking at group %s." % group_id)
